648f0512431b0a198d74179ab9632d47e69b0929
Thanh Nguyen <tienthanh8490@gmail.com>
2015-06-22 12:40:07 +0800

    HTTP uppercaserer

---
+++ learnyounode/httpuppercaserer.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var map = require('through2-map');
+var port = process.argv[2];
+
+var server = http.createServer(function(request, response){
+	if (request.method !== 'POST'){
+		return response.end('Only POST request is accepted');
+	}
+	request.pipe(map(function(chunk){
+		return chunk.toString().toUpperCase();
+	})).pipe(response);
+});
+
+server.listen(port);
